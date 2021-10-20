3eb06c2def373d1373da1643650345815908b5d1
Eric RANDRETSARIMANANA <eric_ra@absys.fr>
2021-10-20 17:49:09 +0200

    last

--- src/controller/RefreshToken.ts
+++ src/controller/RefreshToken.ts
@@ -1,10 +1,14 @@
 import mongoose from 'mongoose';
 import { RefreshTokenSchema } from '../model/RefreshToken';
+import { v4 as uuidv4 } from 'uuid';
 
 const RefreshToken = mongoose.model("RefreshToken", RefreshTokenSchema);
 
 const createToken = (user: any): void => {
-    // const refreshToken = new RefreshToken({
-    //     token: 
-    // })
+    let expiredAt = new Date();
+    let _token = uuidv4();
+
+    const refreshToken = new RefreshToken({
+        token: _token
+    })
 } 
